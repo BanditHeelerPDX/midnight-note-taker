86925c74fc8dda6706444a8e5c5b3f044f703771
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-04-08 22:34:23 -0700

    troubleshooting some syntax and punctuation errors, in addition to file path errors

--- Develop/server.js
+++ Develop/server.js
@@ -6,12 +6,15 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use('/api', api);
+app.use('/', api);
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/pages/index.html'))
+);
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
 );
 
 app.get('*', (req, res) => 
