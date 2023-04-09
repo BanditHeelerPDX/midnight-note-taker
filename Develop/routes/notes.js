3ae2850556a1aeb5a1da922f75d600a3554f2e3d
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-04-08 21:31:41 -0700

    notes routes written and 404 worked on

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,1 +1,17 @@
-const notes = require('express').Router();
+// const notes = require('express').Router();
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+app.get('/api/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    const newNote = req.body;
+    newNote.id = uuidv4();
+    notes.push(newNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+    res.json(newNote);
+});
