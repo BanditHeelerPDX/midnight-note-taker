758a9646432cbdf268c439ea3a6eb9b41b50b822
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-04-09 20:01:08 -0700

    all paths correct, all required elements functional

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,17 +1,16 @@
 const notes = require('express').Router();
 const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
 
 
-notes.get('/api/notes', (req, res) => {
+
+notes.get('/notes', (req, res) => {
     const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
     res.json(notes);
 });
 
-notes.post('/api/notes', (req, res) => {
+notes.post('/notes', (req, res) => {
     const notes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
     const newNote = req.body;
-    newNote.id = uuidv4();
     notes.push(newNote);
     fs.writeFileSync('./db/db.json', JSON.stringify(notes));
     res.json(newNote);
